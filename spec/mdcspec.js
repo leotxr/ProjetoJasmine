c6c8850af10ba927d4f1de43da2e04a51cd3d3aa
leotxr <leonardof15@hotmail.com>
2020-05-14 22:43:41 -0300

    Commit 1

---
+++ spec/mdcspec.js
@@ -0,0 +1,8 @@
+describe("mdc", function(){
+    it("mdc", function(){
+        var expResult = 3;
+        var result = mdc(9, 3);
+        alert(result);
+        expect(expResult).toEqual(result);
+    });
+});
